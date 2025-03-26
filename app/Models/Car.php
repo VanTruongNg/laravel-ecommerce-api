41029431a12aae4a49d9d7c86c48474491911ef8
hiu1412 <144113591+hiu1412@users.noreply.github.com>
2025-03-26 16:45:59 +0700

    CRUD Car (Migration, Model, Controller, Route, missing Image)

---
+++ app/Models/Car.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Car extends Model
+{
+    use HasFactory;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'id',
+        'make',
+        'model',
+        'registration',
+        'engine_size',
+        'price',
+    ];
+}
--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,25 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarController;
 
 Route::prefix('auth')->group(function () {
     // Public routes
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
-    
+
     // Protected routes
     Route::middleware(\App\Http\Middleware\JwtMiddleware::class)->group(function () {
         Route::get('user', [AuthController::class, 'user']);
         Route::post('refresh', [AuthController::class, 'refresh']);
         Route::post('logout', [AuthController::class, 'logout']);
     });
+});
+
+Route::prefix('cars')->group(function () {
+    Route::get('/list', [CarController::class, 'index']);
+    Route::post('/create', [CarController::class, 'store']);
+    Route::get('/{id}', [CarController::class, 'show']);
+    Route::put('/update/{id}', [CarController::class, 'update']);
+    Route::delete('/{id}', [CarController::class, 'destroy']);
 });
